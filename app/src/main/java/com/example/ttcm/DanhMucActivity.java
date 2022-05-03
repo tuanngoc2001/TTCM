d9ea2178bb2fa4c55a048953eedb687fad2f792f
hung.vm.dev <hungvumanh2001@gmail.com>
2022-05-03 21:40:49 +0700

    commit hungvm

---
+++ app/src/main/java/com/example/ttcm/DanhMucActivity.java
@@ -0,0 +1,14 @@
+package com.example.ttcm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DanhMucActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_muc);
+    }
+}
