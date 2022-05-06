8254252f608161b161a8cda63fd7d6c4a25220c8
Nguyễn Tuấn Ngọc <72445778+tuanngoc2001@users.noreply.github.com>
2022-05-06 21:02:31 +0700

    add checklogin

--- app/src/main/java/com/example/ttcm/activity/LoginActivity.java
+++ app/src/main/java/com/example/ttcm/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.ttcm.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.TimeAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +12,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.ttcm.R;
+import com.example.ttcm.model.im_User;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -27,28 +32,29 @@ public class LoginActivity extends AppCompatActivity {
         edtEmail = findViewById(R.id.edtEmail);
         edtPass = findViewById(R.id.edtPass);
         btnLogin = findViewById(R.id.btnSignUp);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("im_User")
 
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Toast.makeText(LoginActivity.this,document.getData().toString(),Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            //Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("im_User")
+                        .whereEqualTo("username",edtEmail.getText().toString())
+                        .whereEqualTo("password",edtPass.getText().toString())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                                        startActivity(intent);
+                                    }
+                                }
+                                else {
+                                }
+                            }
 
-                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
+                        });
             }
         });
     }
--- app/src/main/java/com/example/ttcm/activity/MainActivity.java
+++ app/src/main/java/com/example/ttcm/activity/MainActivity.java
@@ -59,22 +59,7 @@ public class MainActivity extends AppCompatActivity {
         adddrawer();
         addEven();
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("im_User")
-                .whereEqualTo("username", "admin")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Toast.makeText(MainActivity.this,document.getData().toString(),Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                           //Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
+
     }
 
     private void addEven() {
